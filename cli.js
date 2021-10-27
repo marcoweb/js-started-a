b1704f610a47c1b45e3094d69a9662e3585b6788
marcoweb <marcocarvalho.web@gmail.com>
2021-10-27 12:22:35 +0000

    Operadores

--- cli.js
+++ cli.js
@@ -1,1 +1,15 @@
-console.log("Olá Mundo!!!");
+//let num = 10;
+
+//console.log(++num);
+//console.log(num);
+//console.log("Olá Mundo!!!");
+
+let periodo = 'Manhã'
+
+console.log((periodo == 'Manhã') ? 'Bom Dia' : 'Boa Tarde')
+
+if(periodo == 'Manhã') {
+    console.log('Bom Dia')
+} else {
+    console.log('Boa Tarde')
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+// var teste = "Primeira Variável";
+// let teste_let = "Variável com Let";
+// const teste_const = "Variável Const";
+
+console.log("Olá Mundo 2!!!");
+
+var mensagem = "Testando";
+
+if(true) {
+    let mensagem = "Olá VAR";
+}
+
+document.querySelector("#msg").innerHTML = mensagem;
